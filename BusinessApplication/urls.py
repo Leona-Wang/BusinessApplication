d90c51eff3484e350153babfff6538cca745d9a9
Leona-Wang <leonawang920913@gmail.com>
2024-12-08 00:00:25 +0800

    2412072355
    主頁完成80%(按鈕按下網址是錯的)

--- BusinessApplication/urls.py
+++ BusinessApplication/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('function/', include('functions.urls')),
+    path('', include('functions.urls')),
 ]
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BusinessApplication
-123
+html檔案最前面加{% load static %}
+調用static資料夾內檔案：{% static '相對路徑' %}
+ex：{% static 'vendor/fontawesome-free/css/all.min.css' %}
+run server：python manage.py runserver
